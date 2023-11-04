cc396daa394a15b1453be79845f2bb064d3bfdd9
imparag28 <paragj2801@gmail.com>
2023-11-04 23:02:46 +0530

    producton URL change

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketch-board-3h9n.onrender.com' : 'http://localhost:3001'
 
-export const socket = io('http://localhost:3001');
+export const socket = io(URL);
